901840e064b7eac245bdd3e7f64bd780ead9a6a9
Felipe Torres <felipet73@hotmail.com>
2025-09-25 00:31:50 -0500

    env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  baseUrl: 'http://localhost:3000/api',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtcWx2anF0amhlcWNic2RxbWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNzY1NDcsImV4cCI6MjA3Mjc1MjU0N30.-1rrLYykaMQThtVVD0qvsyemN41mqaomv47YjQoLeFI',
+  SUPABASE_URL: 'https://smqlvjqtjheqcbsdqmhd.supabase.co',
+  SYNCFUSION_KEY:'Ngo9BigBOggjHTQxAR8/V1JFaF5cXGRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXZfc3VdQ2JeUkFzWUVWYEg=',
+  production: false,
+};
